6af077056ff62466f3232041f64740cf766e95b1
Briheet Singh Yadav <briheetyadav@gmail.com>
2024-05-21 01:01:25 +0530

    readdir

--- test.c
+++ test.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <sys/stat.h>
 #define FUSE_USE_VERSION 31
 #define _FILE_OFFSET_BITS 64
@@ -14,7 +15,28 @@ int hello_getattr(const char *path, struct stat *statbuf) {
   return 0;
 }
 
-static const struct fuse_operations hello_oper = {.getattr = hello_getattr};
+int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+                  off_t offset, struct fuse_file_info *fi) {
+  (void)offset;
+  (void)fi;
+
+  struct stat file_statbuf;
+  file_statbuf.st_mode = S_IFREG | 0644;
+
+  struct stat dir_statbuf;
+  dir_statbuf.st_mode = S_IFDIR | 0755;
+
+  filler(buf, "test", &dir_statbuf, 0);
+  filler(buf, "test2", &dir_statbuf, 0);
+  filler(buf, "test3", &file_statbuf, 0);
+
+  return 0;
+}
+
+static const struct fuse_operations hello_oper = {
+    .getattr = hello_getattr,
+    .readdir = hello_readdir,
+};
 
 int main(int argc, char *argv[]) {
   int ret;
