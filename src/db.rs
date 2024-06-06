14e3a0331ea7c3c9bffccd9d2369c5295e9f63e5
Briheet Singh Yadav <briheetyadav@gmail.com>
2024-06-06 19:12:10 +0530

    test_db

--- src/db.rs
+++ src/db.rs
@@ -1,12 +1,23 @@
 use rusqlite::Connection;
-use std::path::Path;
-struct Db {
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+#[derive(Debug)]
+pub struct Db {
+    path: PathBuf,
     connection: Connection,
 }
 
 impl Db {
-    fn new(db_path: &Path) -> Db {
-        let connection = Connection::open(db_path).unwrap();
-        Db { connection }
+    pub fn new(path: PathBuf) -> Db {
+        if !path.exists() {
+            fs::create_dir_all(&path).unwrap();
+        }
+        let sqlite_path = path.join("metadata.db");
+        let connection = Connection::open(sqlite_path).unwrap();
+        Db { path, connection }
     }
+
+    pub fn create_item() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use db::Db;
+use std::path::Path;
 mod db;
+
 fn main() {
-    println!("Hello, world!");
+    let db_path = std::env::args().nth(1).unwrap();
+    let db = Db::new(db_path.into());
+    println!("{:?}", db);
 }
